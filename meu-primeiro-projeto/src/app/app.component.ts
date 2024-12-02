233486f19c64d440b2c7b4fa680e30b994b62211
devLeandroCoelho <devleandrocoelho@gmail.com>
2024-12-02 01:04:50 -0300

    Segundo Componente RODAPÉ

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SaudacaoComponent } from './saudacao/saudacao.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [SaudacaoComponent],
+  imports: [SaudacaoComponent, RodapeComponent, RouterOutlet],
 })
 export class AppComponent {
   title = 'meu-primeiro-projeto';
